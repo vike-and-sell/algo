8aaf86065e8f4de0f6cf4a22456159b739ae0534
lgrymaloski <larissa9474@icloud.com>
2024-07-09 18:36:41 -0700

    fixing app.py so it is the same as main

--- elastic-layer/app.py
+++ elastic-layer/app.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, request
 import os
 from elasticsearch import Elasticsearch
 import urllib3
+import json
 from search import *
 import update
-from recommend import *
+import recommend
 
 
-#http = urllib3.PoolManager()
+http = urllib3.PoolManager()
 
 # init flask app
 app = Flask(__name__)
@@ -18,7 +19,7 @@ username = 'elastic'
 password = os.environ['ELASTIC_PASSWORD']
 
 elastic_client = Elasticsearch(
-    "http://elasticsearch:9200", ##TODO: revert
+    "http://elasticsearch-master:9200",
     basic_auth=(username, password)
 )
 
@@ -35,8 +36,8 @@ except:
 
 
 #from backend gateway file
-#DATA_URL = os.environ["DATA_URL"]
-#DATA_API_KEY = os.environ["DATA_API_KEY"]
+DATA_URL = os.environ["DATA_URL"]
+DATA_API_KEY = os.environ["DATA_API_KEY"]
 
 #eventually add one for updating current listings
 
@@ -44,40 +45,40 @@ def execute_data_request(http: urllib3.PoolManager, path, method, body):
     headers = {
         "X-Api-Key": DATA_API_KEY,
     }
-    return http.request(method, f"http://{DATA_URL}{path}", json=body, headers=headers)
+    result =  http.request(method, f"http://{DATA_URL}{path}", body=body, headers=headers)
+    #response.data ##gives us something
+    return json.loads(result.data.decode('utf-8'))
+
+
 
 
 # Helper functions
 def loadListings():
-    #listings =  execute_data_request(http, path='/get_all_listings', method="GET", body=Non
-    
+    listings =  execute_data_request(http, path='/get_all_listings', method="GET", body=None)
+
     # for now use static test data
-    file = open('test_listings2.json')
-    listings = json.load(file)
-    file.close
+    # file = open('test_listings.json')
+    # listings = json.load(file)
+    # file.close
 
     update.loadElastic(elastic_client, 'listing', 'listing_id', listings)
 
 
 def loadUsers():
-    #users  = execute_data_request(http, path='/get_all_users', method="GET", body=None)
+    users  = execute_data_request(http, path='/get_all_users', method="GET", body=None)
 
     # until backend hooked up
-    file = open('test_users.json')
-    users = json.load(file)
-    file.close
+    # file = open('test_users.json')
+    # users = json.load(file)
+    # file.close
 
     update.loadElastic(elastic_client,'user', 'user_id', users)
 
 
 def getSearchHistory(userid):
     search_history = execute_data_request(http, path=f"/get_search_history?userId={userid}", method="GET",  body=None)
-
-    #Cold Start when User has no History
-    if search_history == []:
-        search_history = ['bike', 'lamp']
-
-    #update.loadElastic(elastic_client, 'search_history', 'user_id', search_history) # get specific name of id
+    
+    #search_history = [{"search_date":"2024-01-01T00:00:00","search_text":"bike"}]
     
     return search_history
 
@@ -86,6 +87,7 @@ def getSearchHistory(userid):
 # search path
 #sample call: "localhost:4500/search?q=here+are+some+terms&type=user"
 # NOTE: when calling this in command line with curl, may have to put url in " " to prevent zsh shell from thinking we're using special characters
+# will need to add to search history eventually
 @app.route('/search', methods=['GET'])
 def test_search():
     #TODO:
@@ -115,19 +117,14 @@ def test_get_rec():
 
     userId = request.args.get('userId')
     loadListings()
-    #UNCOMMENT THIS WHEN integrating
-    #search_history = getSearchHistory(userId)
-    
-    search_history = ['pants']
-
-    results = recommend_algo(elastic_client, search_history)
-
+    search_history = getSearchHistory(userId)
+    results = recommend.recommend_algo(elastic_client, search_history)
     # return results in JSON format
     return results
 
 
 # TODO: SPRINT3:  update preferences call (block for now)
-# PATH: POST /recommendations/{listingId}/ignore
+# PATH: POST /recommendations/ignore?listingId=123&userId=1
 
 
 
